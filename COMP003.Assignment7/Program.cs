b48614a6129f5f0c9977bca7f4ec4740dc4dc0eb
oscar <oscar@LAPTOP-G1N9MG0D>
2024-03-01 23:44:11 -0800

    Add project files.

---
+++ COMP003.Assignment7/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Author: Oscar Mendez
+ * Course: COMP-003A
+ * Purpose: To replicate the outcome for the assignment
+ */
+
+namespace COMP003.Assignment7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array CharacterCounter Section
+            SectionSeparator();
+            Console.WriteLine("Array - CharacterCounter Section");
+            SectionSeparator();
+            Console.Write("Please enter a letter");
+            char userChar = Convert.ToChar(Console.ReadLine());
+            Console.Write("Please enter a word:");
+            string userWord = Console.ReadLine();
+            int charCount = CharacterCounter(userChar, userWord);
+            Console.WriteLine($"Character '{userChar}' appears in the word '{userWord}' {charCount} times.");
+            SectionSeparator();
+
+
+
+        }
+
+        static void SectionSeparator()
+        {
+            Console.WriteLine("".PadRight(50,'*'));
+        }
+
+        static int CharacterCounter(char characterInput, string word)
+        {
+            int count = 0;
+            characterInput = char.ToLower(characterInput);
+            word = word.ToLower();
+
+            foreach (char c in word)
+            {
+                if (c == characterInput)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
